a273104209bae27adf82759cbd9b05bdfcbd07e1
Pneuma <57186734+istoga@users.noreply.github.com>
2022-11-23 09:09:14 -0600

    Completed Movie Project

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const search = document.getElementById("search-bar")
-const searchBtn = document.getElementById("search-button")
+let search = document.getElementById("search-bar")
+let searchBtn = document.getElementById("search-button")
 let message = document.getElementById("movie-section")
 const apiKey = "f2bd1f9f"
 let imdbIdArray = []
@@ -9,10 +9,8 @@ let storeMovie = document.querySelector(".store-movies")
 
 function showMessage() {
     message.innerHTML = 
-        `
-            <img id="movie-film" src="img/movie.svg">
-            <h3 id="movie-list-start">Start exploring</h3>
-        `
+        `<img id="movie-film" src="img/movie.svg">
+        <h3 id="movie-list-start">Start exploring</h3>`
 }
 
 showMessage()
@@ -24,52 +22,50 @@ function hideMessageDisplayed() {
 function searchForMovies() {
     if(search.value.length < 3) {
         message.innerHTML = 
-            `
-            <h2>Unable to locate your film.</h2>
-            <h2>Please input at least three characters in the search box above.</h2>
-            `
-    } else if (search.value !== "") {
+            `<h2>Unable to locate your film.</h2>
+            <h2>Please input at least three characters in the search box above.</h2>`
+    } else  {
         searchBtn.disabled = false
         hideMessageDisplayed()
         renderMovies()
         }
 }
-    
+
 function renderMovies() {
     fetch(`https://www.omdbapi.com/?s=${search.value}&t=&type=movie&page=1&apikey=${apiKey}`)
         .then(response =>  response.json())
         .then(data => {
             for (let movie of data.Search) {
                 fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${apiKey}`)
-                .then((res) => res.json())
+                .then((response) => response.json())
                 .then(data => {
-                    allMovies.push(data)
+                    console.log(movie)
+                    // for (let movie of data.Search) {
+                    //     console.log(data.Search) 
                     const movieListing = 
-                        `
-                        <div class="movie-results" id="minus" data-show="true">
+                        `<div class="movie-results" id="minus"         data-show="true">
                             <div class="movie-col-1">
                                 <img src="${data.Poster}" class="movie-size">
                             </div>
-                            <div class="movie-col-2">
+                            <div>
                                 <div class="movie-title-rating">
                                     <h3 class="movie-info">${data.Title}</h3>
-                                    <p><img src="img/star.png" class="icons">${data.Rated}</p>
+                                    <p><img src="img/star.png" class="icons">${data.imdbRating}</p>
                                 </div>
                                 <div class="movie-runtime">
-                                    <p>${data.Runtime}  ${data.Genre}  <button class="store-movies"><img  onclick="getID('${data.imdbID}')" src="img/plus.png" class="icons" data-show="${data.ID}"></button class="add-movie"> Watchlist</p>
+                                    <p>${data.Runtime}  ${data.Genre}  
+                                    <img  onclick="getID('${movie.imdbID}')" src="img/plus.png" class="icons" data-show="${movie.ID}">Watchlist</p>
                                 </div>
                                 <div class="para-plot">
                                     <p>${data.Plot}</p>
                                 </div>
                             </div>
                         </div>
-                        <hr>
-                        `
+                        <hr>`
                     message.innerHTML += movieListing
-                })  
-                
+                })
             }
-        }) 
+        })
 }
 
 function getID(id){
@@ -77,23 +73,28 @@ function getID(id){
         .then((res) => res.json())
         .then(data => {
             btnAddToWatchlist(id)
+            getFromLocalStorage(id)
+            addedChange(id)
         })
 }
+
+// function addedChange(id) {
+//     return 
+//     `<img onclick="getID('movie.imdbID')" src="img/plus.png" class="icons" data-show="undefined"></img>`
+// }
 
 function btnAddToWatchlist(id) {
     for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i)
-        console.log(key)
         imdbIdArray.push(id)
     }
     localStorage.setItem("movies", JSON.stringify(imdbIdArray))
-    console.log(imdbIdArray)
+    getFromLocalStorage(id)
 }
-    // function getFromLocalStorage() {
-//     let storedMovieArray = []
-//     storedMovieArray = localStorage.getItem("movies")
-//     movieArray = JSON.parse(storedMovieArray)
-//     // console.log(storedMovieArray)
-// }
+
+function getFromLocalStorage(id) {
+    storedMovieArray = localStorage.getItem("movies")
+    movieArray = JSON.parse(storedMovieArray)
+}
 
 searchBtn.addEventListener("click", searchForMovies)
--- watchlist.js
+++ watchlist.js
@@ -1,40 +1,46 @@
-//had to make another js file since kept getting null when console logging the watchList element which is on watchlist.html not index.html, don't know if there is a better way to do this or not
-
-
-
 let watchList = document.getElementById("watch-list")
-console.log(watchList)
+const apiKey = "f2bd1f9f"
+let storedMovieArray = []
+let removeBtn = document.getElementById("remove-movie")
+let watchMsg = document.getElementById("watch-message")
+let watchMovieArray = JSON.parse(localStorage.getItem("movies"))
 
+function renderArrayToWatchlist() {
+    let movieToShow = ""
+    for ( let i = 0; i < watchMovieArray.length; i++) {
+        // movieToShow += `
+        //     <h1>${watchMovieArray[i]}</h1>        
+        // `
+        // watchList.innerHTML = movieToShow
+    fetch(`https://www.omdbapi.com/?i=${watchMovieArray[i]}&apikey=${apiKey}`)
+    .then((res) => res.json())
+    .then(data => {
+        movieToShow += 
+            `
+            <h2>${data.Title}</h2>
+            <img src="${data.Poster}">
+            <p>${data.Plot}</p>
+            <button id="remove-movie" onclick="removeMovie('clickedMovie')">Remove</button>
+            <hr>
+            `
+            watchList.innerHTML = movieToShow
+            watchMsg.innerHTML = ""
+    })
+}
+}
 
-function btnAddToWatchlist(movies) {
-    console.log(movies)
-    // storeMovie.innerHTML = movies.imdbID
-    // let listing = movies.imdbID
-    // for (let i = 0; i < movies.length; i++)
-    // listing[0] = movies[i].imdbID
-    // imdbIdArray.push(listing)
-    // localStorage.setItem("movies", JSON.stringify(movies))
-    html = ""
-    html = 
-    `<div class="movie-results" id="minus" data-show="true">
-    <div class="movie-col-1">
-        <img src="${movies.Poster}" class="movie-size">
-    </div>
-    <div class="movie-col-2">
-        <div class="movie-title-rating">
-            <h3 class="movie-info">${movies.Title}</h3>
-            <p><img src="img/star.png" class="icons">${movies.Rated}</p>
-        </div>
-        <div class="movie-runtime">
-            <p>${movies.Runtime}  ${movies.Genre}  <button class="store-movies"><img src="img/plus.png" class="icons" data-show="${movies.ID}"></button class="add-movie"> Watchlist</p>
-        </div>
-        <div class="para-plot">
-            <p>${movies.Plot}</p>
-        </div>
-    </div>
-</div>
-<hr>
-        `
-        watchList += html
+renderArrayToWatchlist()
 
+function removeMovie(clickedMovie) {
+    for ( let i = 0; i < watchMovieArray.length; i++) {
+        let activeElement = watchMovieArray[i]
+        watchMovieArray.splice(activeElement, 1)
+        localStorage.setItem("movies", JSON.stringify(watchMovieArray))
+        if (watchMovieArray == []) {
+            watchList.innerHTML = ``
+            window.location.reload()
+        } else {
+            renderArrayToWatchlist()
+            }
+    }
 }
